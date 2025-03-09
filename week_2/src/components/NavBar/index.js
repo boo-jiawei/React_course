b806b859525066b2ab3a235961dd347243d9c8ad
Chee Jiann Pung <jiaweipung@gmai.com>
2025-03-09 13:05:31 +0800

    Navbars

---
+++ week_2/src/components/NavBar/index.js
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavBarContainer = styled.nav`
+  background: orange;
+  padding: 1rem;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const Navitem = styled.li``;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 1.2rem;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: red;
+  }
+`;
+
+const NavBar = () => {
+  return (
+    <NavBarContainer>
+      <NavList>
+        <Navitem>
+          <NavLink to="/">Home</NavLink>
+        </Navitem>
+        <Navitem>
+          <NavLink to="about">About</NavLink>
+        </Navitem>
+        <Navitem>
+          <NavLink to="contact">Contact</NavLink>
+        </Navitem>
+      </NavList>
+    </NavBarContainer>
+  );
+};
+
+export default NavBar;
